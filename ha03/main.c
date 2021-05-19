7e85a6b75d7b741d4d04a030473f5769d495c5bc
KoKoKotlin <labruzzler@gmail.com>
2021-05-19 16:21:18 +0200

    backup

--- ha03/main.c
+++ ha03/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int decompress()
 {
-    FILE *stdin_handle  = fdopen(0, "r");
+    FILE *stdin_handle  = fdopen(0, "rb");
 	FILE *stdout_handle = fdopen(1, "w");
+
 
 	int byte;
 	while ((byte = fgetc(stdin_handle)) != EOF) {
@@ -39,12 +41,90 @@ error:
 	return -1;
 }
 
+enum compression_state {
+	META,
+	HOMOGENEOUS,
+	HETEROGENEOUS
+};
+
+void output_het(char *buffer, FILE *stdout_handle) {
+	unsigned char status = strlen(buffer);
+	if (status == 0x00) return;
+
+	fwrite(&status, 1, 1, stdout_handle);
+	fwrite(buffer, strlen(buffer), 1, stdout_handle);
+}
+
+void output_hom(char c, int count, FILE *stdout_handle) {
+	unsigned char status = 0x80 | count;
+
+	fwrite(&status, 1, 1, stdout_handle);
+	fputc(c, stdout_handle);
+}
+
 int compress()
 {
-    FILE *stdin_handle  = fdopen(0, "r");
-	FILE *stdout_handle = fdopen(1, "w");
+    FILE *stdin_handle  = fdopen(0, "rb");
+	FILE *stdout_handle = fdopen(1, "wb");
 
-    //TODO
+	enum compression_state current_state = META;
+
+	char buffer[128];
+	memset(buffer, 0, 128);
+
+	size_t count = 0;
+
+	char x = fgetc(stdin_handle);
+	char x_1 = fgetc(stdin_handle);
+
+	while (x != EOF) {
+		switch (current_state) {
+		case META:
+			count = 0;
+			memset(buffer, 0, 128);
+
+			if (x == x_1) {
+				count++;
+				current_state = HOMOGENEOUS;
+			} else {
+				buffer[count++] = x;
+				current_state = HETEROGENEOUS;
+			}
+			break;
+		case HOMOGENEOUS:
+			count++;
+			if (x == x_1) {
+				if (count >= 127) {
+					output_hom(x, count, stdout_handle);
+					current_state = META;
+				}
+			} else {
+				output_hom(x, count, stdout_handle);
+				current_state = HETEROGENEOUS;
+				count = 0;
+				memset(buffer, 0, 128);
+			}
+			break;
+		case HETEROGENEOUS:
+			if (x != x_1) {
+				buffer[count++] = x;
+
+				if (count >= 127 || x_1 == EOF) {
+					output_het(buffer, stdout_handle);
+					current_state = META;
+				}
+			} else {
+				output_het(buffer, stdout_handle);
+				memset(buffer, 0, 128);
+				current_state = HOMOGENEOUS;
+				count = 1;
+			}
+			break;
+		}
+
+		x = x_1;
+		x_1 = fgetc(stdin_handle);
+	}
 
     fclose(stdin_handle);
 	fclose(stdout_handle);
@@ -58,8 +138,8 @@ int main(int argc, char **argv)
         switch (opt) {
             case 'd':
                 return decompress();
-            default:
-                return compress();
         }
     }
+
+	return compress();
 }
