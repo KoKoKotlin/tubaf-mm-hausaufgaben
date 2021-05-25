a25d720b9bae1770179796dd2dcccdd854b599c2
KoKoKotlin <labruzzler@gmail.com>
2021-05-25 19:36:07 +0200

    removed script

--- ha03/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-md5sum samples/picture.bmp && ./honkpack < samples/picture.bmp | ./honkpack -d | md5sum
-md5sum samples/text.txt && ./honkpack < samples/text.txt | ./honkpack -d | md5sum
-md5sum samples/document.pdf && ./honkpack < samples/document.pdf | ./honkpack -d | md5sum
